be62aa87b1d056f3933208c5de9f4648fbd6c0ff
Alek Novkovski <alek@greatcode2.com>
2023-02-10 20:06:59 +0100

    Add actions to Cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -16,6 +16,10 @@ const Cart = (props) => {
         <span>Total Amount</span>
         <span>35.62</span>
       </div>
+      <div className={classes.actions}>
+        <button className={classes['button--alt']}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
     </div>
   );
 };
