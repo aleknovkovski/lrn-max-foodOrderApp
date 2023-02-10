521cf308574d618a1cd1f18f585dcfa76bc5bbf9
Alek Novkovski <alek@greatcode2.com>
2023-02-10 16:09:25 +0100

    Create Dummy AvailableMeals; Insert into App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/Layout/Header";
 import MealsSummary from "./components/Meals/MealsSummary";
+import AvailableMeals from "./components/Meals/AvailableMeals";
 
 function App() {
     return (<>
         <Header/>
         <main>
             <MealsSummary/>
+            <AvailableMeals/>
         </main>
     </>);
 }
---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,7 @@
+const AvailableMeals = () => {
+  return (
+    <h1>Available Meals here</h1>
+  );
+};
+
+export default AvailableMeals;
