0c8e00660a44a29285f75435f5f9ad629780e783
Alek Novkovski <alek@greatcode2.com>
2023-02-11 00:55:35 +0100

    Show number of items in Badge

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,24 @@
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
+import useCartContext from "../../store/cartContext";
 
-function HeaderCartButton (props) {
-  return (
-    <button className={classes.button} onClick={props.onButtonClick}>
-      <span className={classes.icon}>
-        <CartIcon />
-      </span>
-      <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
-    </button>
-  );
-};
+function HeaderCartButton(props) {
+    const {items} = useCartContext()
+    const numberOfCartItems = items.reduce((curNumber, item) => {
+        return curNumber + item.amount;
+    }, 0);
+
+
+    return (
+        <button className={classes.button} onClick={props.onButtonClick}>
+           <span className={classes.icon}>
+               <CartIcon/>
+           </span>
+            <span>Your Cart</span>
+            <span className={classes.badge}>{numberOfCartItems}</span>
+        </button>
+    );
+}
+
 
 export default HeaderCartButton;
