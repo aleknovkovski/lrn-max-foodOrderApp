27c35bce5a6a13f6569284f6978d61d15b14af5e
Alek Novkovski <alek@greatcode2.com>
2023-02-10 21:03:40 +0100

    Move Modal wrapping within Cart

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
-import Modal from "./components/UI/Modal";
 import Cart from "./components/Cart/Cart";
 
 function App() {
     return (<>
-        <Modal><Cart/></Modal>
+        <Cart/>
         <Header/>
         <main>
             <Meals/>
