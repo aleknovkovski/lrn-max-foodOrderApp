710f77d273efb25a6881b2b773c37b23a9104077
Alek Novkovski <alek@greatcode2.com>
2023-02-10 21:05:40 +0100

    Hide Cart by default w cartIsShown state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import {useState} from "react";
+
 
 function App() {
+
+    const [cartIsShown, setCartIsShown] = useState(false);
+
     return (<>
-        <Cart/>
+        {cartIsShown && <Cart/>}
         <Header/>
         <main>
             <Meals/>
