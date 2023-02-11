2218bf1ef50ad089f5db2fc69937653825c4d7c8
Alek Novkovski <alek@greatcode2.com>
2023-02-11 14:30:25 +0100

    Prettier reformat

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,27 +2,27 @@ import classes from './Cart.module.css';
 import Modal from "../UI/Modal";
 
 const Cart = (props) => {
-  const cartItems = (
-    <ul className={classes['cart-items']}>
-      {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => (
-        <li>Item: {item.name}</li>
-      ))}
-    </ul>
-  );
+    const cartItems = (
+        <ul className={classes['cart-items']}>
+            {[{id: 'c1', name: 'Sushi', amount: 2, price: 12.99}].map((item) => (
+                <li>Item: {item.name}</li>
+            ))}
+        </ul>
+    );
 
-  return (
-    <Modal closeThisModal={props.closeCart}>
-      {cartItems}
-      <div className={classes.total}>
-        <span>Total Amount</span>
-        <span>35.62</span>
-      </div>
-      <div className={classes.actions}>
-        <button className={classes['button--alt']} onClick={props.closeCart}>Close</button>
-        <button className={classes.button}>Order</button>
-      </div>
-    </Modal>
-  );
+    return (
+        <Modal closeThisModal={props.closeCart}>
+            {cartItems}
+            <div className={classes.total}>
+                <span>Total Amount</span>
+                <span>35.62</span>
+            </div>
+            <div className={classes.actions}>
+                <button className={classes['button--alt']} onClick={props.closeCart}>Close</button>
+                <button className={classes.button}>Order</button>
+            </div>
+        </Modal>
+    );
 };
 
 export default Cart;
