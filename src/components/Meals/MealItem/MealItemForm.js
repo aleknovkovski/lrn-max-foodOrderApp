8cd511d092a1dd1ad291ba08fdb502c655864a8a
Alek Novkovski <alek@greatcode2.com>
2023-02-11 13:02:14 +0100

    Show message if amount not valid

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -39,6 +39,7 @@ function MealItemForm(props) {
                 }}
             />
             <button>+ Add</button>
+            {!amountIsValid && <p>Please enter a valid amount (1-5).</p>}
         </form>
     );
 };
